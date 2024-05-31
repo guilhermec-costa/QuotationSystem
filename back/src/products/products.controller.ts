badfdce18979e1aff9018524764dec487dd04a03
Guilherme China <echina725@gmail.com>
2024-05-31 20:09:17 -0300

    adds products endpont

--- back/src/products/products.controller.ts
+++ back/src/products/products.controller.ts
@@ -1,7 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Sse } from '@nestjs/common';
 import { ProductsService } from './products.service';
+
+enum Status {
+    available = "In Stock",
+    unavailable = "Out of Stock"
+}
+
+type Product = {
+    id: number,
+    name: string,
+    description: string,
+    price: number,
+    status: Status,
+    quantity: number
+}
+
 
 @Controller('products')
 export class ProductsController {
+    // Products CRUD
     constructor(private readonly productService: ProductsService) { }
+
+    @Get() //   /api/products
+    public findAll(): Product[] { return [] }
+
+    @Get(":id") //   /api/products/id
+    public async findOne(@Param("id") id: number): Promise<number> {
+        return await new Promise((resolve) => {
+            setTimeout(() => resolve(id), 3000)
+        })
+    }
+
+    @Post()
+    public async create(@Body() product: Product): Promise<Product> {
+        return product
+    }
+
 }
+
+
