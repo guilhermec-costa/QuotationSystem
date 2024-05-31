74eb56ccac2032f7e655fba744b39f67894a68b3
Guilherme China <echina725@gmail.com>
2024-05-31 14:52:52 -0300

    understading nest structure

---
+++ back/src/products/products.controller.ts
@@ -0,0 +1,7 @@
+import { Controller, Get } from '@nestjs/common';
+import { ProductsService } from './products.service';
+
+@Controller('products')
+export class ProductsController {
+    constructor(private readonly productService: ProductsService) { }
+}
