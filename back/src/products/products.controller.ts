c2c3f12221ce3ff1b7505d50e49dac62c893a2cd
Guilherme China <echina725@gmail.com>
2024-06-01 10:43:41 -0300

    idk

--- back/src/products/products.controller.ts
+++ back/src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Sse } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, Sse } from '@nestjs/common';
 import { ProductsService } from './products.service';
 
 enum Status {
@@ -22,10 +22,16 @@ export class ProductsController {
     constructor(private readonly productService: ProductsService) { }
 
     @Get() //   /api/products
-    public findAll(): Product[] { return [] }
+    public findAll(@Query("status") status?: Status): Product[] {
+        console.log(status);
+        return [];
+    }
 
     @Get(":id") //   /api/products/id
     public async findOne(@Param("id") id: number): Promise<number> {
+        const x = 5;
+        console.log(x)
+
         return await new Promise((resolve) => {
             setTimeout(() => resolve(id), 3000)
         })
@@ -34,6 +40,16 @@ export class ProductsController {
     @Post()
     public async create(@Body() product: Product): Promise<Product> {
         return product
+    }
+
+    @Patch(":id")
+    public async update(@Param("id") id: number, @Body() product: Product): Promise<{ id: number } & Product> {
+        return { id, ...product };
+    }
+
+    @Delete(":id")
+    public async delete(@Param("id") id: number): Promise<number> {
+        return id;
     }
 
 }
