46a6a8920f8e609ef4ffb3a93fe67f4254a6a48f
Guilherme China <echina725@gmail.com>
2024-06-01 20:47:34 -0300

    deletes products folder

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { ProductModule } from './product/product.module';
@@ -9,7 +8,6 @@ import { ProductModule } from './product/product.module';
 @Module({
     imports: [
         ConfigModule.forRoot({ isGlobal: true }),
-        ProductsModule,
         DatabaseModule,
         ProductModule
     ],
--- back/src/products/dto/update-product.dto.ts
+++
@@ -1,4 +0,0 @@
-import { CreateProductDTO } from "./create-product.dto";
-import { PartialType } from "@nestjs/mapped-types";
-
-export class UpdateProductDTO extends PartialType(CreateProductDTO) { }
