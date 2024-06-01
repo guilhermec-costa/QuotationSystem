e07d51d442a45cf3c49b8c1ccf4fc4d7f27e97ac
Guilherme China <echina725@gmail.com>
2024-06-01 19:00:04 -0300

    fix

--- back/src/database/database.service.ts
+++ back/src/database/database.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 
 @Injectable()
-export class DatabaseService {}
+export class DatabaseService { }
