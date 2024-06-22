81dfb90ef52968326bd7312d843f9ba8f001caa8
Guilherme China <echina725@gmail.com>
2024-06-22 13:39:36 -0300

    removes backend fix

--- back/src/quotation/dto/update-quotation.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateQuotationDto } from './create-quotation.dto';
-
-export class UpdateQuotationDto extends PartialType(CreateQuotationDto) {}
